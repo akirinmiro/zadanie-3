dedf3d161b5fc38cc5ca20530ce880c94fe5bfc1
akirin <akirin@HOME-PC>
2024-10-14 22:32:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Human.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Human(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public static IEnumerable<Human> Where(IEnumerable<Human> humans, Func<Human, bool> predicate)
+        {
+            foreach (var human in humans)
+            {
+                if (predicate(human))
+                {
+                    yield return human;
+                }
+            }
+        }
+        public static IEnumerable<Human> OrderBy(IEnumerable<Human> humans, Func<Human, int> keySelector)
+        {
+            return humans.OrderBy(keySelector);
+        }
+    
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleApp7;
+
+
+
+    var humans = new List<Human>
+{
+    new Human("Джон", 25),
+    new Human("Алиса", 30),
+    new Human("Ольга", 20),
+    new Human("Ева", 35),
+    new Human("Катя", 28)
+};
+
+    var filteredHumans = humans.Where(h => h.Age > 25);
+    Console.WriteLine("Люди старше 25 лет:");
+    foreach (var human in filteredHumans)
+    {
+        Console.WriteLine($"Имя: {human.Name}, Возраст: {human.Age}");
+    }
+
+
+    var sortedHumans = humans.OrderBy(h => h.Name.Length);
+    Console.WriteLine("\nЛюди, отсортированные по длине имени:");
+    foreach (var human in sortedHumans)
+    {
+        Console.WriteLine($"Имя: {human.Name}, Возраст: {human.Age}");
+    }
+
+
